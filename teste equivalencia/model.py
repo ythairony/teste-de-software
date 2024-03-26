4b47988b358f6ff8e240bdac2fec80472e18ded2
Yuri Thairony <yurithairony@hotmail.com>
2024-03-26 18:28:41 -0300

    Finalizado os testes de equivalência

--- teste equivalencia/model.py
+++ teste equivalencia/model.py
@@ -1,12 +1,13 @@
 # modelo.py
-import datetime
-
 class Triangulo:
     
     def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+        if a > 0 and b > 0 and c > 0:
+            self.a = a
+            self.b = b
+            self.c = c
+        else:
+            raise TypeError('Não é um triângulo válido')
 
     # Para formar um triângulo o valor de cada lado deve ser menor que a soma dos outros 2 lados.
     # ‘a’, ‘b’, e ‘c’ devem formam um triângulo, a função retorna true
@@ -22,14 +23,15 @@ class Triangulo:
     
     # Triângulo Equilátero: três lados iguais;
     def ehEquilatero(self):
-        if (self.a == self.b and self.a == self.c):
-            return True
+        if self.validarForma():
+            if (self.a == self.b and self.a == self.c):
+                return True
 
         return False
     
     # Triângulo Isósceles: quaisquer dois lados iguais;
     def ehIsosceles(self):
-        if(not self.ehEquilatero()):
+        if self.validarForma():
             if ((self.a == self.b and self.a != self.c) or (self.a == self.c and self.a != self.b) or (self.b == self.c and self.a != self.b)):
                 return True
 
@@ -37,7 +39,8 @@ class Triangulo:
 
     # Triângulo Escaleno: três lados diferentes;
     def ehEscaleno(self):
-        if (self.a != self.b and self.a != self.c and self.b != self.c):
-            return True
+        if self.validarForma():
+            if (self.a != self.b and self.a != self.c and self.b != self.c):
+                return True
 
         return False 
---
+++ teste equivalencia/test.py
@@ -0,0 +1,133 @@
+import unittest
+
+from model import Triangulo
+
+
+class TrianguloTest(unittest.TestCase):
+    def test_ehTriangulo(self):
+        a, b, c = 10, 15, 20
+        t = Triangulo(a, b, c)
+
+        self.assertTrue(t.validarForma())
+
+
+    def test_naoEhTriangulo(self):
+        a, b, c = 10, 50, 20
+        t = Triangulo(a, b, c)
+
+        self.assertFalse(t.validarForma())
+
+
+    def test_anguloMenorQueZero(self):
+        a,b,c = -1, -2, 3
+
+        with self.assertRaises(TypeError):
+            t = Triangulo(a,b,c)
+
+
+class TrianguloEquilateroTest(unittest.TestCase):
+    def test_ehEquilatero(self):
+        a, b, c = 10, 10, 10
+        t = Triangulo(a,b,c)
+
+        self.assertTrue(t.ehEquilatero)
+
+
+    def test_naoEhEquilatero(self):
+        # A != C
+        a,b,c = 10, 10, 15
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEquilatero())
+
+        # A != B
+        a,b,c = 15, 10, 15
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEquilatero())
+
+        # B != C
+        a,b,c = 10, 10, 15
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEquilatero())
+
+
+        # Não ser triângulo
+        a,b,c = 10, 50, 15
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEquilatero())
+
+
+
+class TrianguloIscolesTest(unittest.TestCase):
+    def test_ehIsoceles(self):
+        a,b,c = 10, 10, 15
+        t = Triangulo(a,b,c)
+
+        self.assertTrue(t.ehIsosceles())
+
+
+    def test_naoEhIsoceles(self):
+        # A = B e A = C
+        a,b,c = 10, 10, 10
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehIsosceles())
+
+        # A != B e B != C
+        a,b,c = 10, 15, 20
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehIsosceles())
+
+        # Não ser triangulo
+        a,b,c = 10, 50, 15
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEquilatero())
+
+
+class TrianguloEscalenoTest(unittest.TestCase):
+    def test_ehEscaleno(self):
+        a,b,c = 10, 15, 20
+        t = Triangulo(a,b,c)
+
+        self.assertTrue(t.ehEscaleno)
+
+
+    def test_naoEhEscaleno(self):
+        # A = B e A != C
+        a,b,c = 10, 10, 15
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEscaleno())
+
+        # A = C e A != B
+        a,b,c = 10, 15, 10
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEscaleno())
+
+        # B = C e A != B
+        a,b,c = 15, 10, 10
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEscaleno())
+
+        # A = B e A = C
+        a,b,c = 10, 10, 10
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEscaleno())
+
+        # Não ser triangulo
+        a,b,c = 10, 50, 15
+        t = Triangulo(a,b,c)
+
+        self.assertFalse(t.ehEquilatero())
+
+
+if __name__ == "__main__":
+    unittest.main()
