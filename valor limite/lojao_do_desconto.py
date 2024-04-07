170322bbaff4782904f3a029de16007e2f3d17e4
Yuri Thairony <yurithairony@hotmail.com>
2024-04-06 21:52:49 -0300

    Testes criados da atividade de desconto

--- valor limite/lojao_do_desconto.py
+++ valor limite/lojao_do_desconto.py
@@ -1,7 +1,7 @@
 class Desconto:
-    def __init__(self, valor: float, idade: int):
-        self.valor = valor
-        self.idade = idade
+    def __init__(self):
+        self.valor = 0
+        self.idade = 0
         self._desconto = 0
 
     '''
@@ -16,30 +16,30 @@ class Desconto:
     
 
     # Verifica se o valor é elegível a desconto
-    def _isElegivel(self):
-        if self.idade >= 0 and self.idade <= 24 and self.valor >= 250:
+    def _isElegivel(self, valor:float, idade:int):
+        if idade >= 0 and idade <= 24 and valor >= 250:
             return True
         return False
 
 
     # Calcula o desconto válido
-    def _calcularDesconto(self):
-        if self.idade <= 12:
+    def _calcularDesconto(self, idade):
+        if idade <= 12:
             self._desconto = 0.15
         
-        elif self.idade <= 18:
+        elif idade <= 18:
             self._desconto = 0.12
 
-        elif self.idade <= 21:
+        elif idade <= 21:
             self._desconto = 0.05
 
-        elif self.idade <= 24:
+        elif idade <= 24:
             self._desconto = 0.03
 
     
     # Valida se o desconto 
-    def _validarDesconto(self):
-        if self._isElegivel():
-            self._calcularDesconto()
-            valor_com_desconto = self.valor * (1 - self.desconto)
+    def _validarDesconto(self, valor:float, idade:int):
+        if self._isElegivel(valor, idade):
+            self._calcularDesconto(idade)
+            valor_com_desconto = valor * (1 - self._desconto)
             return valor_com_desconto
--- valor limite/test.py
+++ valor limite/test.py
@@ -2,65 +2,182 @@ import unittest
 
 from lojao_do_desconto import Desconto
 
+
 class DescontoTest(unittest.TestCase):
     # Idade e valor válido
     def test_IdadeEValorValido(self):
-        compra = Desconto(250, 0)
-        self.assertTrue(compra._isElegivel())
+        valor = 250
+        idade = 0
+        compra = Desconto()
+        self.assertTrue(compra._isElegivel(valor, idade))
         
-        compra = Desconto(251, 1)
-        self.assertTrue(compra._isElegivel())
+        valor = 251
+        idade = 1
+        compra = Desconto()
+        self.assertTrue(compra._isElegivel(valor, idade))
 
-        compra = Desconto(251, 23)
-        self.assertTrue(compra._isElegivel())
+        valor = 251
+        idade = 23
+        compra = Desconto()
+        self.assertTrue(compra._isElegivel(valor, idade))
 
-        compra = Desconto(251, 24)
-        self.assertTrue(compra._isElegivel())
+        valor = 251
+        idade = 24
+        compra = Desconto()
+        self.assertTrue(compra._isElegivel(valor, idade))
 
 
     # Idade e valor inválido
     def test_IdadeEValorInvalido(self):
-        compra = Desconto(249, -1)
-        self.assertFalse(compra._isElegivel())
+        valor = 249
+        idade = -1
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
 
-        compra = Desconto(249, 25)
-        self.assertFalse(compra._isElegivel())
+        valor = 249
+        idade = 25
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
 
 
     # Idade válida e valor inválido
     def test_IdadeValidaValorInvalido(self):
-        compra = Desconto(249, 0)
-        self.assertFalse(compra._isElegivel())
+        valor = 249
+        idade = 0
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
 
-        compra = Desconto(249, 1)
-        self.assertFalse(compra._isElegivel())
+        valor = 249
+        idade = 1
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
         
-        compra = Desconto(249, 23)
-        self.assertFalse(compra._isElegivel())
+        valor = 249
+        idade = 23
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
 
-        compra = Desconto(249, 24)
-        self.assertFalse(compra._isElegivel())
+        valor = 249
+        idade = 24
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
 
 
     # Idade inválida e valor válido
     def test_IdadeInvalidaValorValido(self):
-        compra = Desconto(250, -1)
-        self.assertFalse(compra._isElegivel())
+        valor = 250
+        idade = -1
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
 
-        compra = Desconto(251, -1)
-        self.assertFalse(compra._isElegivel())
+        valor = 251
+        idade = -1
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
 
-        compra = Desconto(250, 25)
-        self.assertFalse(compra._isElegivel())
+        valor = 250
+        idade = 25
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
 
-        compra = Desconto(251, 25)
-        self.assertFalse(compra._isElegivel())
+        valor = 251
+        idade = 25
+        compra = Desconto()
+        self.assertFalse(compra._isElegivel(valor, idade))
 
 
-    def test_CalcularDescontoIdadeMenorQue12Anos(self):
-        compra = Desconto(250, 0)
-        self.assertTrue(compra._calcularDesconto())
+    def test_ValidarDescontoIdadeMenorQue12Anos(self):
+        # Valor com desconto = 212,5
+        valor = 250
+        idade = 0
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertEqual(valor_com_desconto, 212.5)
 
+        valor = 250
+        idade = 12
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertEqual(valor_com_desconto, 212.5)
+
+
+    def test_ValidarDescontoIdadeMenorQue18Anos(self):
+        # Valor com desconto = 220
+        valor = 250
+        idade = 13
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertEqual(valor_com_desconto, 220)
+
+        valor = 250
+        idade = 18
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertEqual(valor_com_desconto, 220)
+
+
+    def test_ValidarDescontoIdadeMenorQue21Anos(self):
+        # Valor com desconto = 237,5
+        valor = 250
+        idade = 19
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertEqual(valor_com_desconto, 237.5)
+
+        valor = 250
+        idade = 21
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertEqual(valor_com_desconto, 237.5)
+
+
+    def test_ValidarDescontoIdadeMenorQue24Anos(self):
+        # Valor com desconto = 242,5
+        valor = 250
+        idade = 22
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertEqual(valor_com_desconto, 242.5)
+
+        valor = 250
+        idade = 24
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertEqual(valor_com_desconto, 242.5)
+
+
+    def test_DescontoNaoValidadoPelaIdade(self):
+        valor = 250
+        idade = 25
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertIsNone(valor_com_desconto)
+
+
+    def test_DescontoNaoValidadoPeloValor(self):
+        valor = 249
+        idade = 24
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertIsNone(valor_com_desconto)
+
+        valor = 249
+        idade = 21
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertIsNone(valor_com_desconto)
+
+        valor = 249
+        idade = 18
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertIsNone(valor_com_desconto)
+
+        valor = 249
+        idade = 12
+        compra = Desconto()
+        valor_com_desconto = compra._validarDesconto(valor, idade)
+        self.assertIsNone(valor_com_desconto)
 
 
 if __name__ == "__main__":
