35ad38147c46f22fd12a1cee4feee56f31923f48
Yuri Thairony <yurithairony@hotmail.com>
2024-07-07 12:25:54 -0300

    Atividade de TDD

--- tdd/fracao.py
+++ tdd/fracao.py
@@ -1,13 +1,47 @@
 import math
 
+
 class Fracao:
     def __init__(self, numerador, denominador):
         if denominador == 0:
             raise ValueError('Denominador deve ser diferente de zero')
 
         # Reduzindo numerador e denominador 
-        self._numerador = numerador / math.gcd(numerador, denominador)
-        self._denominador = denominador / math.gcd(numerador, denominador)
+        self._numerador = numerador // math.gcd(numerador, denominador)
+        self._denominador = denominador // math.gcd(numerador, denominador)
+
+
+
+    def somar(self, f):
+        if self.denominador == f.denominador:
+            return Fracao(self.numerador + f.numerador, self.denominador)
+        else: 
+            novo_numerador = self.numerador * f.denominador + f.numerador * self.denominador
+            novo_denominador = self.denominador * f.denominador
+            return Fracao(novo_numerador, novo_denominador)
+        
+
+    def subtrair(self, f):
+        if self.denominador == f.denominador:
+            return Fracao(self.numerador - f.numerador, self.denominador)
+        else: 
+            novo_numerador = self.numerador * f.denominador - f.numerador * self.denominador
+            novo_denominador = self.denominador * f.denominador
+            return Fracao(novo_numerador, novo_denominador)
+
+
+    def multiplicar(self, f):
+        novo_num = self.numerador * f.numerador
+        novo_dem = self.denominador * f.denominador
+        return Fracao(novo_num, novo_dem)
+    
+
+    def dividir(self, f):
+        novo_num = self.numerador * f.denominador
+        novo_dem = self.denominador * f.numerador
+        return Fracao(novo_num, novo_dem)
+
+
 
     @property
     def numerador(self):
--- tdd/test.py
+++ tdd/test.py
@@ -2,6 +2,7 @@ import unittest
 
 from fracao import Fracao
 
+
 class FracaoTest(unittest.TestCase):
     def test_construtor_1(self):
         f = Fracao(2,3)
@@ -19,7 +20,41 @@ class FracaoTest(unittest.TestCase):
         self.assertEqual(1, f.numerador)
         self.assertEqual(2, f.denominador)
 
+
+    # TESTE ADIÇÃO
+    def test_adicao(self):
+        f1 = Fracao(3, 5)
+        f2 = Fracao(1, 5)
+        res = f1.somar(f2)
+        self.assertEqual(4, res.numerador)
+        self.assertEqual(5, res.denominador)
         
+
+    # TESTE SUBTRAÇÃO
+    def test_subtracao(self):
+        f1 = Fracao(3, 5)
+        f2 = Fracao(1, 5)
+        res = f1.subtrair(f2)
+        self.assertEqual(2, res.numerador)
+        self.assertEqual(5, res.denominador)
+
+
+    # TESTE MULTIPLICAÇÃO
+    def test_multiplicacao(self):
+        f1 = Fracao(3, 5)
+        f2 = Fracao(1, 5)
+        res = f1.multiplicar(f2)
+        self.assertEqual(3, res.numerador)
+        self.assertEqual(25, res.denominador)
+        
+
+    # TESTE DIVISÃO 
+    def test_divisao(self):
+        f1 = Fracao(3, 5)
+        f2 = Fracao(1, 5)
+        res = f1.dividir(f2)
+        self.assertEqual(3, res.numerador)
+        self.assertEqual(1, res.denominador)
 
 if __name__ == '__main__':
     unittest.main()
