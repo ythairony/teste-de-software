c878d37072db64d0f8fec5f03cae750ccf6cb92e
Yuri Thairony <102562875+ythairony@users.noreply.github.com>
2024-07-09 10:54:13 +0000

    separando metodo do test

---
+++ TP4 - TDD/celsius.py
@@ -0,0 +1,2 @@
+def convert_celsius_to_fahrenheit(celsius):
+    return celsius * (9/5) + 32 
--- TP4 - TDD/test_conversao.py
+++ TP4 - TDD/test_conversao.py
@@ -1,7 +1,5 @@
 import unittest 
-
-def convert_celsius_to_fahrenheit(celsius):
-    return celsius * (9/5) + 32 
+from celsius import convert_celsius_to_fahrenheit
 
 
 class TestTemperatureConversion(unittest.TestCase):
