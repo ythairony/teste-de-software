c243b1a83c12a029c0da59b3a0ae0cba869ad8e6
Yuri Thairony <102562875+ythairony@users.noreply.github.com>
2024-07-11 11:06:03 +0000

    implementando fatorial

--- TP4 - TDD/test_fatorial.py
+++ TP4 - TDD/test_fatorial.py
@@ -3,18 +3,17 @@ from fatorial import factorial
 
 class TestFatorial(unittest.TestCase):
     def test_factorial_positivo(self):
-        n = factorial(self)
+        n = factorial(3)
         self.assertEqual(6, n)
         pass
 
     def test_factorial_zero(self):
-        n = factorial(self)
+        n = factorial(0)
         self.assertEqual(1, n)
 
     def test_factorial_negativo(self):
         with self.assertRaises(ValueError):
-            n = 0
-            t = factorial(n)
+            t = factorial(-1)
 
 
 if __name__ == '__main__':
